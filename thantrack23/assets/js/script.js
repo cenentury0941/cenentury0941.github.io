84e64f71a3e097d916ffd3c16beaa8621f4a2fb1
Cenentury0941 <67197834+cenentury0941@users.noreply.github.com>
2023-05-24 23:15:19 +0530

    Added Basic Responsiveness

--- thantrack23/assets/js/script.js
+++ thantrack23/assets/js/script.js
@@ -3,5 +3,48 @@ setTimeout(
         document.getElementById("init").style.animationName = "hideTitleBack";
         document.getElementById("init").style.animationIterationCount = 1;
     } ,
-    3900
+    2500
 );
+
+
+
+var characters = {}
+var char_api = "https://gateway.marvel.com:443/v1/public/characters";
+var public_key = "b501936f0cdd8d3278fc9ec70db1ad7a";
+var private_key = "7c686a1135c99ae7bb45811c5a31f01c4f9d108c";
+var ts = "1";
+
+function initialiseHeros()
+{
+    for( var i = 0 ; i < 6 ; i++ )
+    {
+        var name = characters[i].name;
+        var ico_url = characters[i].thumbnail.path + "." + characters[i].thumbnail.extension ;
+
+        document.getElementById("hero"+i).innerHTML = "<h4>"+name+"</h4>";
+        document.getElementById("icon"+i).style.backgroundImage = "url("+ico_url+")" ;
+    }
+}
+
+async function getCharacters()
+{
+
+    var tohash = ts + private_key + public_key ;
+
+    let hash = md5(tohash);
+    console.log(hash);
+
+    char_api += "?ts=1&apikey=" + public_key + "&hash=" + hash + "&limit=6" + "&offset=" + Math.floor(Math.random()*100) ;
+
+    const response = await fetch(char_api);
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+    characters = jsonData.data.results;
+    console.log(characters);
+
+    initialiseHeros();
+
+}
+
+getCharacters();
