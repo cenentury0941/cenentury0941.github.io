2b58a3d22cc755243e2c5dd7ba8f0a44ec5ade91
Cenentury0941 <67197834+cenentury0941@users.noreply.github.com>
2023-05-24 21:03:28 +0530

    Added MilkyWay

---
+++ thantrack23/assets/js/script.js
@@ -0,0 +1,7 @@
+setTimeout(
+    () => {
+        document.getElementById("init").style.animationName = "hideTitleBack";
+        document.getElementById("init").style.animationIterationCount = 1;
+    } ,
+    3900
+);
